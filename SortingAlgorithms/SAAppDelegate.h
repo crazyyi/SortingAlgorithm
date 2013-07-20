6e42bcf6f98ba03e03dc9032c41a7ffde3d5294f
crazyyi <gdzack@gmail.com>
2013-07-20 00:29:44 -0700

    Initial Commit

---
+++ SortingAlgorithms/SAAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SAAppDelegate.h
+//  SortingAlgorithms
+//
+//  Created by Yi Zeng on 20/07/13.
+//  Copyright (c) 2013 AFun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SAAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SortingAlgorithms/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SortingAlgorithms
+//
+//  Created by Yi Zeng on 20/07/13.
+//  Copyright (c) 2013 AFun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SAAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SAAppDelegate class]));
+    }
+}
